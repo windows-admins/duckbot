44610c403fb562c6b035a74d0e3b78839acc2be7
Krys <75592214+Kooties@users.noreply.github.com>
2023-01-25 11:32:02 -0500

    Update events.go

--- bot/events.go
+++ bot/events.go
@@ -83,9 +83,9 @@ func handlePlusMinus(item string, operation string, s *discordgo.Session, m *dis
 
 	var plural string
 	if score == 1 {
-		plural = "partridge in a pear tree"
+		plural = "fuzzy bunny"
 	} else {
-		plural = "partridges in a pear tree"
+		plural = "fuzzy bunnies"
 	}
 	if user == nil {
 		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%[1]s has %[2]d %[3]s", item, score, plural))
