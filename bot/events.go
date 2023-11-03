121423b2bec75bfdf4aa5edb99be8c5ec9cb1dfe
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2023-11-03 13:43:12 -0400

    Updating points name

--- bot/events.go
+++ bot/events.go
@@ -83,9 +83,9 @@ func handlePlusMinus(item string, operation string, s *discordgo.Session, m *dis
 
 	var plural string
 	if score == 1 {
-		plural = "firework"
+		plural = "WSUS database"
 	} else {
-		plural = "fireworks"
+		plural = "WSUS databases"
 	}
 	if user == nil {
 		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%[1]s has %[2]d %[3]s", item, score, plural))
